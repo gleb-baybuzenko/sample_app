284220b7b55a130752dbbdb1b6b9edd137b6d9a0
gleb.baybuzenko <gleb.baybuzenko@gmail.com>
2016-07-27 11:30:21 +0300

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+# Factory for the User model objects simulation
+FactoryGirl.define do
+  factory :user do
+    name     "Michael Hartl"
+    email    "michael@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
